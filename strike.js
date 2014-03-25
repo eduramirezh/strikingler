9358d127fc479524afcca9bd53936ab88ad883b9
Eduardo Ramírez <ejramire@uc.cl>
2014-03-25 12:14:58 -0300

    conf file base, now testing

--- strike.js
+++ strike.js
@@ -3,10 +3,11 @@ var request = require('request');
 var cheerio = require('cheerio');
 var parser = require('./parser.js');
 var fs = require('fs');
-
+var _ = require('lodash');
 //load
 var url = process.argv[2];
 var filename = process.argv[3] || "strikingled.html";
+var conf;
 request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     //replace relative links
@@ -18,6 +19,18 @@ request(url, function (error, response, body) {
     $('#strikingly-footer-logo').remove();
     $('navbar-drawer.strikingly-drawer.hidden').remove();
     $('.socialMedia.content').remove();
+    
+    //conf file
+    fs.readFile('./strike.conf','utf8',function(err, data){
+      if(err){
+        return;
+      }
+      conf = JSON.parse(data);
+      _.forEach(conf.url.modifyHTML,function(elem){
+        $(elem.query).html(elem.newValue);
+      } );
+    } );
+
 
     //save file
     fs.writeFile(filename, $.html(), function(err){

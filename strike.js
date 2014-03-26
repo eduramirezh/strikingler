659f253b117c3f36af89b386ea695256e7d39382
Eduardo Ramírez <ejramire@uc.cl>
2014-03-25 21:53:02 -0300

    conf file working

--- strike.js
+++ strike.js
@@ -1,19 +1,19 @@
 //load dependencies
 var request = require('request');
 var cheerio = require('cheerio');
-var parser = require('./parser.js');
 var fs = require('fs');
 var _ = require('lodash');
 //load
 var url = process.argv[2];
 var filename = process.argv[3] || "strikingled.html";
 var conf;
+
 request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     //replace relative links
     body = body.split("=\"//").join("=\"http://");
     body = body.split("='//").join("='http://");
-
+  
     //load cheerio and remove unnecesary divs
     var $ = cheerio.load(body);   
     $('#strikingly-footer-logo').remove();
@@ -21,18 +21,15 @@ request(url, function (error, response, body) {
     $('.socialMedia.content').remove();
     
     //conf file
-    fs.readFile('./strike.conf','utf8',function(err, data){
-      if(err){
-        return;
-      }
-      conf = JSON.parse(data)[url];
-      if (conf.title){
-        $('title').html(conf.title);
-      }
-      if (conf.favicon){
-        $('link[href="http://assets.strikingly.com/assets/favicon.ico"]').attr('href', conf.favicon);
-      }
-    } );
+    conf = JSON.parse(fs.readFileSync('./strike.json','utf8'))[url];
+    console.log("conf =");
+    console.log(conf);
+    if (conf.title){
+      $('title').html(conf.title);
+    }
+    if (conf.favicon){
+      $('link[href="http://assets.strikingly.com/assets/favicon.ico"]').attr('href', conf.favicon);
+    }
 
 
     //save file
